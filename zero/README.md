2e253c6d90fcd39030ab8f3ae087935bf49f10ad
zbian <kurisusnowdeng@gmail.com>
2022-03-03 22:56:56 +0800

    added zero benchmark

---
+++ zero/README.md
@@ -0,0 +1,25 @@
+# GPT2 ZeRO Benchmark
+GPT2 ZeRO benchmark with data parallelism to evaluate Colossal-AI, DeepSpeed, FairScale and PatrickStar.
+
+## Setup
+1. Install dependencies
+```
+pip install -r requirement.txt
+```
+2. Download PatrickStar from github
+```
+https://github.com/Tencent/PatrickStar.git
+```
+3. Install PatrickStar
+```
+cd PatrickStar
+pip install .
+```
+
+## Usage
+1. Prepare datasets and tokenizers from HuggingFace Hub if necessary (e.g. we provide an example of training `wikitext-2`).
+
+2. Run benchmark with one of the systems to evaluate
+```
+DATA=/PATH/TO/DATASET TOKENIZER=/PATH/TO/TOKENIZER LOG=/PATH/TO/LOG torchrun --nproc_per_node=NUM_GPUS run.py --config=CONFIG_FILE
+```
---
+++ zero/deepspeed_utils/utils.py
@@ -0,0 +1,24 @@
+def init_w_ds(builder, config):
+    import deepspeed
+
+    deepspeed.init_distributed()
+
+    build_data, build_model, build_loss, build_optimizer, build_scheduler = builder()
+
+    train_data, test_data = build_data()
+
+    with deepspeed.zero.Init(config_dict_or_path=config):
+        model = build_model()
+
+    criterion = build_loss()
+
+    optimizer = build_optimizer(model.parameters())
+
+    lr_scheduler = build_scheduler(len(train_data), optimizer)
+
+    model, optimizer, _, lr_scheduler = deepspeed.initialize(model=model,
+                                                             optimizer=optimizer,
+                                                             lr_scheduler=lr_scheduler,
+                                                             config=config)
+
+    return model, train_data, test_data, criterion, optimizer, None, lr_scheduler
