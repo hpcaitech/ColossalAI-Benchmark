bcec6049295ee02d1c2bb7d2ebe482007ff645a1
zbian <kurisusnowdeng@gmail.com>
2022-03-11 15:14:59 +0800

    updated usage and configs

--- zero/colossalai_utils/utils.py
+++ zero/colossalai_utils/utils.py
@@ -1,21 +1,17 @@
-<<<<<<< HEAD
-import torch
-import torch.distributed as dist
 from common.utils import CONFIG, print_log
-=======
-from common.utils import CONFIG
->>>>>>> b90b3ab... rollbacked flops profiler to hardcoding; reworked how cpu adam is used for different methods
+from torch.cuda import max_memory_allocated, reset_peak_memory_stats
+from torch.distributed import get_rank
 
 
 def init_w_col(builder):
     import colossalai
     from colossalai.core import global_context as gpc
     from colossalai.logging import disable_existing_loggers
+    from colossalai.nn.optimizer import CPUAdam
     # from colossalai.zero.init_ctx import ZeroInitContext
     from colossalai.zero.shard_utils import TensorShardStrategy
     from colossalai.zero.sharded_model import ShardedModel, ShardedModelV2
     from colossalai.zero.sharded_optim import ShardedOptimizerV2
-    from colossalai.nn.optimizer import CPUAdam
 
     disable_existing_loggers()
     colossalai.launch_from_torch(config=CONFIG)
@@ -26,21 +22,10 @@ def init_w_col(builder):
 
     use_v2 = gpc.config.zero.pop('version', 2) == 2
 
-<<<<<<< HEAD
-    print_log('Building model')
-    if use_v2:
-        shard_strategy = TensorShardStrategy()
-        # with ZeroInitContext(convert_fp16='fp16' in gpc.config,
-        #                      target_device=torch.device(gpc.config.zero.offload_config.device),
-        #                      shard_strategy=shard_strategy,
-        #                      shard_param=True):
-        #     model = build_model()
-        model = build_model()
-    else:
-        model = build_model()
-=======
     cpu_offload = gpc.config.zero.offload_config.device == 'cpu'
 
+    rank = get_rank()
+    reset_peak_memory_stats(rank)
     # if use_v2:
     shard_strategy = TensorShardStrategy()
     # with ZeroInitContext(convert_fp16='fp16' in gpc.config,
@@ -48,7 +33,6 @@ def init_w_col(builder):
     #                      shard_strategy=shard_strategy,
     #                      shard_param=True):
     model = build_model()
->>>>>>> b90b3ab... rollbacked flops profiler to hardcoding; reworked how cpu adam is used for different methods
 
     if use_v2:
         model = ShardedModelV2(model, shard_strategy, **gpc.config.zero)
@@ -56,12 +40,9 @@ def init_w_col(builder):
         model = ShardedModel(model, **gpc.config.zero)
 
     criterion = build_loss()
-<<<<<<< HEAD
-    print_log(
-        f'GPU Mem: {torch.cuda.max_memory_allocated(dist.get_rank()) / (1024 * 1024)} M')
-    print_log('Building optimizer')
-    optimizer = build_optimizer(model.parameters())
-=======
+
+    print_log(f'Peak Memory = {max_memory_allocated(rank) / (1024 * 1024)} M')
+    reset_peak_memory_stats(rank)
 
     if cpu_offload:
         optimizer = CPUAdam(model.parameters(),
@@ -69,7 +50,6 @@ def init_w_col(builder):
                             weight_decay=CONFIG['hyperparameter']['weight_decay'])
     else:
         optimizer = build_optimizer(model.parameters())
->>>>>>> b90b3ab... rollbacked flops profiler to hardcoding; reworked how cpu adam is used for different methods
 
     lr_scheduler = build_scheduler(len(train_data), optimizer)
 
@@ -80,6 +60,6 @@ def init_w_col(builder):
                                        **gpc.config.get('fp16', dict()),
                                        cpu_offload=cpu_offload)
 
-    print_log(
-        f'GPU Mem: {torch.cuda.max_memory_allocated(dist.get_rank()) / (1024 * 1024)} M')
+    print_log(f'Peak Memory = {max_memory_allocated(rank) / (1024 * 1024)} M')
+
     return model, train_data, test_data, criterion, optimizer, None, lr_scheduler
