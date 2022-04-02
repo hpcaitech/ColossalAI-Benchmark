21ab7bcfe579487bc30353be5d65bc9a58fdf0e8
Wesley <ziyue.jiang97@gmail.com>
2022-04-02 16:26:48 +0800

    refine code

--- bert/colossalai_utils/model_zoo/__init__.py
+++ bert/colossalai_utils/model_zoo/__init__.py
@@ -1,3 +1,3 @@
-from .colo_tp1dcol_bert import create_colo_bert_pipeline_model, ColoBertForMaskedLM, ColoBertMaskedLMLoss
+from .colo_bert import create_colo_bert_pipeline_model, ColoBertForMaskedLM, ColoBertMaskedLMLoss
 
 __all__ = ['create_colo_bert_pipeline_model', 'ColoBertForMaskedLM', 'ColoBertMaskedLMLoss']
--- bert/colossalai_utils/utils.py
+++ bert/colossalai_utils/utils.py
@@ -8,10 +8,8 @@ def init_w_col(builder):
     from colossalai.core import global_context as gpc
     from colossalai.nn.optimizer import CPUAdam
     from colossalai.zero.init_ctx import ZeroInitContext
-    from colossalai.zero.shard_utils import (BucketTensorShardStrategy,
-                                             TensorShardStrategy)
-    from colossalai.zero.sharded_model import ShardedModelV2
-    from colossalai.zero.sharded_optim import ShardedOptimizerV2
+    from colossalai.zero.shard_utils import (BucketTensorShardStrategy)
+    from colossalai.amp import AMP_TYPE
 
     colossalai.launch_from_torch(config=CONFIG)
 
